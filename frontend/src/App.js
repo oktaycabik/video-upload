2d1e21d3b0fbdb0f87e89d7a157da5b0651df91b
oktaycabik <oktaycabik2@gmail.com>
2025-02-16 01:21:28 +0300

    qweqw

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<div>Ana Sayfaaaaa</div>} />
+          <Route path="/" element={<div>Ana Sayfaaaaaa</div>} />
           <Route path="/upload" element={<VideoUpload />} />
         </Routes>
       </div>
